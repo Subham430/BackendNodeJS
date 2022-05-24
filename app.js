18a1b3f3268165afe3324f4ab19fc673b5ef2893
Subham Dutta <subham.heritage@gmail.com>
2022-05-24 11:14:05 +0530

    swagger installed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 // const cookieParser = require("cookie-parser");
 const path = require("path");
 const body = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./src/config/swagger');
 const express = require('express');
 const app = express();
 
@@ -11,7 +13,7 @@ app.use(express.urlencoded({ extended:true }));
 
 const {notFoundHandler, errorHandler} = require('./middlewares/common/errorHandler')
 
-const router = require("./routers");
+const router = require("./routes");
 app.use('/swastik', router);
 
 //not found handler
@@ -19,5 +21,6 @@ app.use(notFoundHandler);
 
 // default error handler
 app.use(errorHandler);
+app.use(process.env.SWAGGER_DOC_URL, swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
 
 app.listen(process.env.PORT, () => console.log(`Example app is listening on port ${process.env.PORT}`));
